3b2ff08eadbac71b1197d44a555d82384603e32a
Liubomir Stanev <lyubomir.y.stanev.2019@elsys-bg.org>
2023-03-09 15:59:31 +0200

    Nano/Slave module

---
+++ Slave module/Slave_Nano/Slave_Nano.ino
@@ -0,0 +1,79 @@
+#include <Arduino.h>
+#include <stdio.h>
+
+#define ON 1
+#define OFF 0
+#define pirPin 2
+#define sensor 3
+#define LED 4
+#define laser 5
+
+int vibration_Sensor = A5;
+int calibrationTime = 5;
+long unsigned int lowIn;
+long unsigned int pause = 5000;
+boolean lockLow = true;
+boolean takeLowTime;
+int PIRValue = 0;
+int present_condition = 0;
+int previous_condition = 0;
+
+int counter = 0;
+
+void setup() {
+   Serial.begin(9600);
+   pinMode(pirPin, INPUT);
+   pinMode(LED, OUTPUT);
+   pinMode(sensor, INPUT);
+   pinMode(laser, OUTPUT);
+   digitalWrite(laser, HIGH);
+}
+
+void loop() {
+   Serial.println(counter);
+   PIRSensor();
+   bool value = digitalRead(sensor);
+    if (value == 0) 
+    {
+      counter ++;
+      delay(1000);
+    }
+   
+   previous_condition = present_condition;
+   present_condition = digitalRead(vibration_Sensor);
+   if (previous_condition != present_condition) 
+    {
+      counter ++;
+      delay(1000);
+    }
+    if (counter >= 10) {
+    digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
+    
+  }
+}
+
+void PIRSensor() {
+   if(digitalRead(pirPin) == HIGH) {
+      if(lockLow) {
+         PIRValue = 1;
+         lockLow = false;
+         counter ++;
+         delay(1000);
+      }
+      takeLowTime = true;
+   }
+   if(digitalRead(pirPin) == LOW) {
+      if(takeLowTime){
+         lowIn = millis();
+         takeLowTime = false;
+      }
+      if(!lockLow && millis() - lowIn > pause) {
+         PIRValue = 0;
+         lockLow = true;
+         delay(50);
+      }
+   }
+   
+}
