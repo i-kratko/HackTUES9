8522d166543377197fff56485238b2676c332f8f
Nikolay Yordanov <nikolay.i.yorddanov.2019@elsys-bg.org>
2023-03-09 16:17:26 +0200

    First half of the master module done

---
+++ MasterModule/MasterModule.ino
@@ -0,0 +1,54 @@
+//Ultrasonic sensor #1
+const int pingPin1 = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin1 = 6; // Echo Pin of Ultrasonic Sensor
+//Ultrasonic sensor #2
+const int pingPin2 = 8; // Trigger Pin of Ultrasonic Sensor
+const int echoPin2 = 9; // Echo Pin of Ultrasonic Sensor
+const int ledPin = 13;
+long duration, cm;
+void setup() {
+   Serial.begin(9600); // Starting Serial Terminal
+   pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+   ultraSonic(pingPin1, echoPin1);
+   ultraSonic(pingPin2, echoPin2);
+   temperature();
+}
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
+}
+
+void ultraSonic(int pingPin, int echoPin) {
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   cm = microsecondsToCentimeters(duration);
+   Serial.print(cm);
+   Serial.print("cm");
+   Serial.println();
+
+  if(cm <= 5) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+
+   delay(100);
+}
+
+void temperature() {
+  DHT.read(DHT11_PIN);
+  Serial.print("temp:");
+  Serial.print(DHT.temperature);
+  Serial.println();
+  delay(1000);
+}
